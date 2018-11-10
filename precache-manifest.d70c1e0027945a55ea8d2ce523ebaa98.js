a5cbd031a982c75ad22f055e539bb063a2da49f8
William Lee <william.lee@orionhealth.com>
2018-11-10 19:27:30 +1300

    Updates

--- precache-manifest.d70c1e0027945a55ea8d2ce523ebaa98.js
+++ precache-manifest.d70c1e0027945a55ea8d2ce523ebaa98.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/pictionary/static/js/runtime~main.df1f96c9.js"
   },
   {
-    "revision": "15b63ee1f249b4807a0b",
-    "url": "/pictionary/static/js/main.15b63ee1.chunk.js"
+    "revision": "f6ea4703b68aee2cd886",
+    "url": "/pictionary/static/js/main.f6ea4703.chunk.js"
   },
   {
     "revision": "3af36091c18ba251ef9d",
     "url": "/pictionary/static/js/1.3af36091.chunk.js"
   },
   {
-    "revision": "15b63ee1f249b4807a0b",
+    "revision": "f6ea4703b68aee2cd886",
     "url": "/pictionary/static/css/main.55cb3300.chunk.css"
   },
   {
-    "revision": "bf842ce86fa7ed3a5006e8995a6a8f51",
+    "revision": "dbc4ffc3cbd5d969e9da374ce735d17f",
     "url": "/pictionary/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pictionary/precache-manifest.bcc017416d1a8b695191eb9b1d36ec10.js"
+  "/pictionary/precache-manifest.d70c1e0027945a55ea8d2ce523ebaa98.js"
 );
 
 workbox.clientsClaim();
